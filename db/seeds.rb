d1ea66d84285e2ab4ccbe6cc5d22625e93d11e4c
Jason Malnar <jmmalnar@utexas.edu>
2017-02-07 16:46:53 -0600

    Added a state and city to the seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+City.delete_all
+State.delete_all
+
+City.create!(
+    name: "Baltimore"
+)
+
+State.create!(
+    name: "Maryland"
+)
