5c6d3c959bb2650803806b389a598c90c62a3706
Jason Malnar <jmmalnar@utexas.edu>
2017-03-01 08:44:21 -0600

    Added create, update and delete functionality

--- app/assets/javascripts/spa-demo/cities/cities.controller.js
+++ app/assets/javascripts/spa-demo/cities/cities.controller.js
@@ -11,6 +11,10 @@
         var vm = this;
         vm.cities;
         vm.city;
+        vm.edit = edit;
+        vm.create = create;
+        vm.update = update;
+        vm.remove = remove;
 
         activate();
         return;
@@ -23,18 +27,57 @@
         function newCity() {
             vm.city = new City();
         }
+
         function handleError(response) {
             console.log(response);
         }
-        function edit(object, index) {
+
+        function edit(object) {
+            console.log("selected", object);
+            vm.city = object;
         }
+
         function create() {
+            //console.log("creating city", vm.city);
+            vm.city.$save()
+                .then(function (response) {
+                    //console.log(response);
+                    vm.cities.push(vm.city);
+                    newCity();
+                })
+                .catch(handleError);
         }
+
         function update() {
+            //console.log("update", vm.city);
+            vm.city.$update()
+                .then(function (response) {
+                    //console.log(response);
+                })
+                .catch(handleError);
         }
+
         function remove() {
+            //console.log("remove", vm.city);
+            vm.city.$delete()
+                .then(function (response) {
+                    //console.log(response);
+                    //remove the element from the local array
+                    //removeElement(vm.cities, vm.city);
+                    vm.cities = City.query();
+                    //replace edit area with prototype instance
+                    newCity();
+                })
+                .catch(handleError);
         }
+
         function removeElement(elements, element) {
+            for (var i = 0; i < elements.length; i++) {
+                if (element[i].id == element.id) {
+                    elements.splice(i, 1);
+                    break;
+                }
+            }
         }
     }
 })();
