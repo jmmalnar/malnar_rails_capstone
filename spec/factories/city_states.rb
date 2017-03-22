4df2eac7b7b74b7c4909d3a70d972978bd30f81d
Jason Malnar <jmmalnar@utexas.edu>
2017-03-22 07:59:53 -0500

    Finished setting up FactoryGirl

--- spec/factories/city_states.rb
+++ spec/factories/city_states.rb
@@ -4,6 +4,32 @@ FactoryGirl.define do
     name 'test'
   end
 
-  factory :city, :parent=>:city_fixed do
+  factory :city, :parent => :city_fixed do
+  end
+
+  factory :city_sequence, class: 'City' do
+    sequence(:name) { |n| "test#{n}" }
+  end
+
+  factory :city_names, class: 'City' do
+    sequence(:name) { |n| ["larry", "moe", "curly"][n%3] }
+  end
+
+  factory :city_transient, class: 'City' do
+    transient do
+      large true
+    end
+
+    after(:build) do |object, props|
+      object.name = props.large ? "Large City" : "Small City"
+    end
+  end
+
+  # For an immutable object
+  factory :city_ctor, class: 'City' do
+    transient do
+      hash {}
+    end
+    initialize_with { City.new(hash) }
   end
 end
