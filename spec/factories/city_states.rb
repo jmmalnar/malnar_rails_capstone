ee62c78ecc5381fe301604608f12053c4ad57cfb
Jason Malnar <jmmalnar@utexas.edu>
2017-03-23 08:00:37 -0500

    More Factory stuff

--- spec/factories/city_states.rb
+++ spec/factories/city_states.rb
@@ -1,10 +1,8 @@
 FactoryGirl.define do
 
+  ##### CITY Factories
   factory :city_fixed, class: 'City' do
     name 'test'
-  end
-
-  factory :city, :parent => :city_fixed do
   end
 
   factory :city_sequence, class: 'City' do
@@ -32,4 +30,17 @@ FactoryGirl.define do
     end
     initialize_with { City.new(hash) }
   end
+
+  factory :city_faker, class: 'City' do
+    name {Faker::Address.city}
+  end
+
+  factory :city, :parent => :city_faker do
+  end
+
+  ##### STATE Factories
+  factory :state, class: 'State' do
+    name { Faker::Address.state}
+  end
+
 end
--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 describe City, type: :model do
-  include_context 'db_cleanup'
+  include_context 'db_cleanup', :transaction
   before(:all) do
-    @city = FactoryGirl.create(:city)
+    @city = FactoryGirl.create(:city, :name=>'test')
   end
   let(:city) { City.find(@city.id) }
 
