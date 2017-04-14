d05e8a2067e6ee0facfb09583d1e40056aa42f77
Jason Malnar <jmmalnar@utexas.edu>
2017-04-14 08:34:53 -0500

    Had to drop db and re-create and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170310142935) do
+ActiveRecord::Schema.define(version: 20170414131245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,60 +22,34 @@ ActiveRecord::Schema.define(version: 20170310142935) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "doctors", force: :cascade do |t|
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "doctors", ["user_id"], name: "index_doctors_on_user_id", using: :btree
-
-  create_table "patient_entries", force: :cascade do |t|
-    t.integer  "pain_level"
-    t.integer  "sample_color"
-    t.float    "drain_quantity"
-    t.text     "note"
-    t.datetime "date_of_sample"
-    t.integer  "patient_id"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-  end
-
-  add_index "patient_entries", ["patient_id"], name: "index_patient_entries_on_patient_id", using: :btree
-
-  create_table "patients", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "doctor_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "patients", ["doctor_id"], name: "index_patients_on_doctor_id", using: :btree
-  add_index "patients", ["user_id"], name: "index_patients_on_user_id", using: :btree
-
   create_table "users", force: :cascade do |t|
-    t.string   "firstname"
-    t.string   "lastname"
+    t.string   "provider",               default: "email", null: false
+    t.string   "uid",                    default: "",      null: false
+    t.string   "encrypted_password",     default: "",      null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,       null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.string   "name"
+    t.string   "nickname"
+    t.string   "image"
     t.string   "email"
-    t.string   "password_digest"
-    t.string   "remember_digest"
-    t.boolean  "admin",             default: false
-    t.string   "activation_digest"
-    t.boolean  "activated",         default: false
-    t.datetime "activated_at"
-    t.string   "reset_digest"
-    t.datetime "reset_sent_at"
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
-    t.string   "address"
-    t.string   "city"
-    t.string   "state"
-    t.string   "zipcode"
-    t.string   "phone"
+    t.json     "tokens"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_foreign_key "doctors", "users"
-  add_foreign_key "patient_entries", "patients"
-  add_foreign_key "patients", "doctors"
-  add_foreign_key "patients", "users"
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
+
 end
